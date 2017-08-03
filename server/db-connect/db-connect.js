fcfd6f7591c09e2594ebcce45246fb80ea13e216
Ravi <ravirkr@icloud.com>
2017-08-03 08:34:18 -0400

    working version in local

--- server/db-connect/db-connect.js
+++ server/db-connect/db-connect.js
@@ -1,4 +1,13 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Product');
-
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://ravi:ravi@ds031872.mlab.com:31872/heroku_6w4f20rq',(e) =>
+{
+  if(e)
+  {
+    return console.log("Error");
+  }
+  else {
+    console.log("Connected");
+  }
+});
 module.export = {mongoose};
